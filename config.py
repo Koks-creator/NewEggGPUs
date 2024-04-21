1f9b11d00ee3edea6a7224ea22ebccca8bbb12ad
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2024-04-21 13:02:15 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 class Config:
     PROJECT_MAIN_PATH: str = Path(__file__).resolve().parent
+    DATABASE_NAME: str = "database"
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///{PROJECT_MAIN_PATH}/database/{DATABASE_NAME}.db"
     SINK_FOLDER: str = "sink"
     LOGS_FOLDER: str = "logs"
     SCRAPE_ELEMENTS = {
