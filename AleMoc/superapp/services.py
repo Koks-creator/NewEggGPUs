a625c7c80122c2b78512e809f1d31f048e70803a
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2024-05-18 00:19:25 +0200

    Update services.py

--- AleMoc/superapp/services.py
+++ AleMoc/superapp/services.py
@@ -22,11 +22,36 @@ def query_table(table_name: str, where: str, columns: list = None) -> pd.DataFra
     return df
 
 
+def create_tooltip(row: pd.Series):
+    return f'{row["ProductTitle"]} ({row["DateCreated"][:-4]})<br>' \
+           f'Price: {row["Price"]}$<br>' \
+           f'Brand: {row["Brand"]}<br>' \
+           f'Series: {row["Series"]}<br>' \
+           f'Model: {row["Model"]}<br>' \
+           f'ChipsetManufacturer: {row["ChipsetManufacturer"]}<br>' \
+           f'GPUSeries: {row["GPUSeries"]}<br>' \
+           f'GPU: {row["GPU"]}<br>' \
+           f'BoostClock: {row["BoostClock"]}<br>' \
+           f'CUDACores: {row["CUDACores"]}<br>' \
+           f'EffectiveMemoryClock: {row["EffectiveMemoryClock"]}<br>' \
+           f'MemorySize: {row["MemorySize"]}<br>' \
+           f'MemoryInterface: {row["MemoryInterface"]}<br>' \
+           f'MemoryType: {row["MemoryType"]}<br>' \
+           f'Interface: {row["Interface"]}<br>' \
+           f'MemorySize: {row["MemorySize"]}<br>' \
+           f'MultiMonitorSupport: {row["MultiMonitorSupport"]}<br>' \
+           f'HDMI: {row["HDMI"]}<br>' \
+           f'DisplayPort: {row["DisplayPort"]}<br>' \
+
+
+
 graph_color_map = {label: np.random.rand(3, ) for label in query_table(table_name="Products", where="")["ProductTitle"]}
 
 if __name__ == '__main__':
     df = query_table(table_name="Products", where="")
-    xd = pd.to_datetime(df["DateCreated"])
-    x = xd.dt.strftime('%Y-%m-%d %H:%M:%S')
+    # spechs = df[["Brand", "Series", "Model", "ChipsetManufacturer", "GPUSeries", "GPU"]]
+    rows = df.iterrows()
+    for ind, row in rows:
+        print(create_tooltip(row))
+        break
 
-    print(x)
