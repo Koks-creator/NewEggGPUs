7637ba53d67c3cb4e5078ce3b9d551b73de47d4d
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2024-05-22 08:11:50 +0200

    adding pie chart

--- AleMoc/superapp/test.py
+++ AleMoc/superapp/test.py
@@ -39,7 +39,12 @@ app.layout = html.Div([
     ], style={'maxHeight': '140px', "height": "140px", "overflow-y": "scroll"}),
     dbc.Button("Apply Filter", id="apply-button", n_clicks=0),
     dcc.Graph(id="graph"),
-    html.Div(id="table", style={"width": "1400px"})
+    dbc.Row([
+        #style={"width": "1400px"}
+        dbc.Col(html.Div(id="table"), width=8),
+        dbc.Col(dcc.Graph(id="ratings-pie-chart"), width=3)
+    ])
+
 ])
 
 
@@ -56,7 +61,8 @@ def update_message(n_clicks, phrase_input):
 @app.callback([Output('dropdown-options', 'options'),
                Output('dropdown-options', 'value'),
                Output('graph', 'figure'),
-               Output('table', 'children')
+               Output('table', 'children'),
+               Output('ratings-pie-chart', 'figure'),
                ],
               [Input("cache", "data"),
                Input('add-phrase-button', 'n_clicks'),
@@ -79,29 +85,34 @@ def update_options(cache, n_clicks, n_clicks2, dataframe_products, reviews_dataf
         selected_options = options.copy()
         selected_options.remove("Select All")
 
+    pie_fig = {}
     fig = {}
     table = dash_table.DataTable()
     if dataframe_products and reviews_dataframe:
         df_products = pd.DataFrame.from_dict(dataframe_products)
         df_products = df_products[df_products["Price"].notna()]
 
-        df_reviews = pd.DataFrame.from_dict(reviews_dataframe)
-
-        # filter
-        df_products = df_products[df_products["ProductTitle"].isin(selected_options)]
-        df_reviews = df_reviews[df_reviews["ProductTitle"].isin(selected_options)]
-        df_reviews["DatePublished"] = pd.to_datetime(df_reviews["DatePublished"]).dt.strftime("%Y-%m-%d")
-        # join
-        df_table = df_reviews.merge(df_products, on="ProductId")
-
-        # Create column with link
-        df_table["Product"] = [f"""[{row[1]["ProductTitle_x"]}]({row[1]["Url"]})""" for row in
-                               df_table[["ProductTitle_x", "Url"]].iterrows()]
-        df_table = df_table[["Product", "Description", "Rating", "DatePublished", "Author"]]
-
         if len(df_products):
             df_products['DateCreated'] = pd.to_datetime(df_products['DateCreated'])
             df_products["DateCreated"] = df_products["DateCreated"].dt.strftime('%Y-%m-%d %H:%M:%')
+            df_products = df_products[df_products["ProductTitle"].isin(selected_options)]
+
+            # Reviews #
+            df_reviews = pd.DataFrame.from_dict(reviews_dataframe)
+            df_reviews = df_reviews[df_reviews["ProductTitle"].isin(selected_options)]
+            df_reviews["DatePublished"] = pd.to_datetime(df_reviews["DatePublished"]).dt.strftime("%Y-%m-%d")
+
+            # join
+            df_table = df_reviews.merge(df_products, on="ProductId")
+
+            # Create column with link
+            df_table["Product"] = [f"""[{row[1]["ProductTitle_x"]}]({row[1]["Url"]})""" for row in
+                                   df_table[["ProductTitle_x", "Url"]].iterrows()]
+            df_table = df_table[["Product", "Description", "Rating", "DatePublished", "Author"]]
+
+            # Ratings #
+            df_ratings = df_reviews.groupby("Rating").count()
+            print(df_ratings.head())
             traces = []
             plot_annotes = []
             # for xi, yi, label, url in zip(x, y, labels, urls):
@@ -146,9 +157,17 @@ def update_options(cache, n_clicks, n_clicks2, dataframe_products, reviews_dataf
                 style_data_conditional=styles.style_data_conditional
             )
 
+            pie_labels = [f"Rating: {rate}" for rate in df_ratings.index.tolist()]
+            pie_fig = go.Figure(
+                data=[
+                    go.Pie(labels=pie_labels, values=df_ratings["Id"].tolist())
+                ]
+            )
+            pie_fig.update_layout(title_text="Ratings Percentage")
+
     return (
         [{'label': option, 'value': option} for option in options],  # Update dropdown options
-        selected_options, fig, table
+        selected_options, fig, table, pie_fig
     )
 
 
